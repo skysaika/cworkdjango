36f73e403f022ee6faeb4e89ee3d3df1b60d6af9
saysuu <skysaika@yandex.ru>
2024-08-19 21:00:59 +0300

    Startapp clients, add to Installed_apps in settings.py, create model Client in models.py, makemigrations and migrate

---
+++ clients/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Client(models.Model):
+    """
+    Client Model - модель клиентов
+    email: почта
+    first_name: имя
+    last_name: фамилия
+    comment: Комментарий
+    """
+    email = models.EmailField(verbose_name='email')
+    first_name = models.CharField(max_length=100, verbose_name='имя')
+    last_name = models.CharField(max_length=100, verbose_name='фамилия')
+    comment = models.CharField(max_length=255, verbose_name='комментарий', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.first_name} ' \
+               f'{self.last_name} ' \
+               f'{self.email}'
+
+    class Meta:
+        verbose_name = 'клиент рассылки'
+        verbose_name_plural = 'клиенты рассылки'
+        ordering = ('email',)
+
