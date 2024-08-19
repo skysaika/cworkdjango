cd34eaa225cbfd9c78f4d1ac1e83b4a521924cc4
saysuu <skysaika@yandex.ru>
2024-08-19 23:08:15 +0300

    Add unique_clients_count to index.html

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from clients.models import Client
+
 
 class IndexView(TemplateView):
     """Представление главной страницы."""
@@ -8,3 +10,10 @@ class IndexView(TemplateView):
     extra_context = {
         'title': 'Главная страница',
     }
+
+    def get_context_data(self, **kwargs):
+        """Добавляем количество уникальных клиентов в контекст."""
+        context = super().get_context_data(**kwargs)
+        unique_clients_count = Client.objects.values('email').distinct().count()
+        context['unique_clients_count'] = unique_clients_count
+        return context
