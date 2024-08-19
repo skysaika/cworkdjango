9b41e35e2026318568b0a884aa436a875f0bc21a
saysuu <skysaika@yandex.ru>
2024-08-20 00:50:35 +0300

    Add ClientUpdateView, def toggle_active, ClientDeleteView in views.py, path: client_edit, active_client, client_delete in urls.py, create button 'Delete' in inc_client_card.html, create client_confirm_delete.html

--- clients/urls.py
+++ clients/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from clients.apps import ClientsConfig
-from clients.views import ClientListView, ClientDetailView, ClientCreateView
+from clients.views import ClientListView, ClientDetailView, ClientCreateView, ClientUpdateView, toggle_active, \
+    ClientDeleteView
 
 app_name = ClientsConfig.name
 
@@ -9,4 +10,7 @@ urlpatterns = [
     path('client_list/', ClientListView.as_view(), name='client_list'),  # список клиентов
     path('client_detail/<int:pk>/', ClientDetailView.as_view(), name='client_detail'),  # просмотр клиента
     path('create_client/', ClientCreateView.as_view(), name='create_client'),  # создание клиента
+    path('client_edit/<int:pk>/', ClientUpdateView.as_view(), name='client_edit'),  # редактирование клиента
+    path('client_delete/<int:pk>/', ClientDeleteView.as_view(), name='client_delete'),  # удаление клиента
+    path('active_clients/<int:pk>/', toggle_active, name='active_clients'),  # активация/деактивация клиента
 ]
--- clients/views.py
+++ clients/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from clients.forms import ClientForm
 from clients.models import Client
@@ -34,3 +34,35 @@ class ClientCreateView(CreateView):
         'title': 'Создание клиента'
     }
     success_url = reverse_lazy('clients:client_list')
+
+
+class ClientUpdateView(UpdateView):
+    """Редактирование клиента"""
+    model = Client
+    form_class = ClientForm  # форма для редактирования клиента
+    template_name = 'clients/client_form.html'
+    extra_context = {
+        'title': 'Редактирование клиента'
+    }
+    success_url = reverse_lazy('clients:client_list')
+
+
+class ClientDeleteView(DeleteView):
+    """Удаление клиента"""
+    model = Client
+    template_name = 'clients/client_confirm_delete.html'
+    extra_context = {
+        'title': 'Удаление клиента'
+    }
+    success_url = reverse_lazy('clients:client_list')
+
+
+def toggle_active(request, pk):
+    """Переключение активности клиента FBV"""
+    client_item = get_object_or_404(Client, pk=pk)
+    if client_item.is_active:
+        client_item.is_active = False
+    else:
+        client_item.is_active = True
+    client_item.save()
+    return redirect(reverse('clients:client_list'))
