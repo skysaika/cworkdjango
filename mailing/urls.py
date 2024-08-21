3d8e700170b633655a4a57c16aae250677ec7dd5
saysuu <skysaika@yandex.ru>
2024-08-21 18:51:23 +0300

    Create classes MessageDetailView, MessageUpdateView in views.py, path to message_edit, message_detail in urls.py, buttons View, Edit in inc_message_card.html, create message_detail.html in templates

--- mailing/urls.py
+++ mailing/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 
 
 from mailing.apps import MailingConfig
-from mailing.views import MessageListView, MessageCreateView
+from mailing.views import MessageListView, MessageCreateView, MessageUpdateView, MessageDetailView
 
 app_name = MailingConfig.name
 
 
 urlpatterns = [
     path('message_list/', MessageListView.as_view(), name='message_list'),  # список сообщений
+    path('message_detail/<int:pk>/', MessageDetailView.as_view(), name='message_detail'),  # просмотр сообщения
     path('message_create/', MessageCreateView.as_view(), name='message_create'),  # создание сообщения
+    path('message_edit/<int:pk>/', MessageUpdateView.as_view(), name='message_edit'),  # редактирование сообщения
 ]
--- mailing/views.py
+++ mailing/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView
 
 from .forms import MessageForm
 from .models import Message
@@ -18,6 +18,17 @@ class MessageListView(ListView):
         return Message.objects.all()
 
 
+class MessageDetailView(DetailView):
+    """Просмотр сообщения"""
+    model = Message
+    template_name = 'mailing/message_detail.html'
+    context_object_name = 'object_list'
+    extra_context = {
+        'title': 'Просмотр сообщения'
+    }
+
+
+
 class MessageCreateView(CreateView):
     """Создание сообщения"""
     model = Message
@@ -25,6 +36,17 @@ class MessageCreateView(CreateView):
     form_class = MessageForm
     extra_context = {
         'title': 'Создание сообщения'
+    }
+    success_url = reverse_lazy('mailing:message_list')
+
+
+class MessageUpdateView(CreateView):
+    """Редактирование сообщения"""
+    model = Message
+    template_name = 'mailing/message_form.html'
+    form_class = MessageForm
+    extra_context = {
+        'title': 'Редактирование сообщения'
     }
     success_url = reverse_lazy('mailing:message_list')
 
