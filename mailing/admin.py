6e0784cb6fbe2f58c7f2ecd339002e698bb354bf
saysuu <skysaika@yandex.ru>
2024-08-22 22:43:15 +0300

    Register @admin.register(Mailing) in mailing/admin.py

--- mailing/admin.py
+++ mailing/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Message
+from .models import Message, Mailing
 
 
 @admin.register(Message)
@@ -8,3 +8,9 @@ class MessageAdmin(admin.ModelAdmin):
     list_display = ('title', 'body', 'is_published')
     list_filter = ('title', 'is_published')
     search_fields = ('title',)
+
+@admin.register(Mailing)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ('send_name', 'message', 'start_time', 'end_time', 'frequency', 'status')
+    list_filter = ('send_name', 'status')
+    search_fields = ('send_name',)
