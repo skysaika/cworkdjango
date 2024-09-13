f74cf1af3939db3a2e66d78e18d746e4a67891ba
saysuu <skysaika@yandex.ru>
2024-09-13 14:16:12 +0300

    register LogAdmin to mailing/admin.py

--- mailing/admin.py
+++ mailing/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Message, Mailing
+from .models import Message, Mailing, Log
 
 
 @admin.register(Message)
@@ -9,8 +9,14 @@ class MessageAdmin(admin.ModelAdmin):
     list_filter = ('title', 'is_published')
     search_fields = ('title',)
 
+
 @admin.register(Mailing)
 class MailingAdmin(admin.ModelAdmin):
     list_display = ('send_name', 'message', 'start_time', 'end_time', 'frequency', 'status')
     list_filter = ('send_name', 'status')
     search_fields = ('send_name',)
+
+
+@admin.register(Log)
+class LogAdmin(admin.ModelAdmin):
+    list_display = ('mailing', 'send_time', 'attempt_status')
