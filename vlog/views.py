92101e2ae2238dd8905f375d6073279eb81acdf5
saysuu <skysaika@yandex.ru>
2024-09-15 17:05:10 +0300

    add owner to model VlogPost, add perms to change post(can publish), add LoginRequiredMixin in views.py

--- vlog/views.py
+++ vlog/views.py
@@ -1,34 +1,47 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.utils.text import slugify
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.http import Http404
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from pytils.translit import slugify
 
 from vlog.models import VlogPost
 
 
 # Create your views here.
-class VlogPostCreateView(CreateView):
+class VlogPostCreateView(LoginRequiredMixin, CreateView):
     """Контроллер создания поста"""
     model = VlogPost
     template_name = 'vlog/vlogpost_form.html'
-    fields = '__all__'
+    fields = ('title', 'content', 'preview', 'is_published')
     success_url = reverse_lazy('vlog:post_list')
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Создание поста'
-        return context
+
+    # def get_object(self, queryset=None):
+    #     """Проверяем что пользователь не может редактировать чужой пост"""
+    #     self.object = super().get_object(queryset)
+    #     if self.object.owner != self.request.user and not self.request.user.is_staff:
+    #         raise Http404('Вы не можете редактировать чужую собаку')  # может владелец и модератор
+    #     return self.object
 
 
-class VlogPostUpdateView(UpdateView):
+class VlogPostUpdateView(LoginRequiredMixin, UpdateView):
     """Контроллер редактирования поста"""
     model = VlogPost
     template_name = 'vlog/vlogpost_form.html'
-    fields = '__all__'
-    success_url = reverse_lazy('vlog:post_list')
+    fields = ('title', 'content', 'preview', 'is_published')
+    # success_url = reverse_lazy('vlog:post_list')
+
+
+
+
+    def get_success_url(self):
+        """Перенаправление после редактирования на страницу поста"""
+        return reverse('vlog:post_detail', args=[self.kwargs.get('pk')])
+
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
         context['title'] = 'Редактирование поста'
         return context
 
@@ -40,15 +53,35 @@ class VlogPostListView(ListView):
     extra_context = {'title': 'Список постов'}
     paginate_by = 4
 
+    def get_queryset(self):
+        """Выводим только опубликованные посты"""
 
-class VlogPostDetailView(DetailView):
+        return VlogPost.objects.filter(is_published=True)
+
+
+class VlogPostDetailView(LoginRequiredMixin, DetailView):
     """Контроллер просмотра поста"""
     model = VlogPost
     template_name = 'vlog/vlogpost_detail.html'
     extra_context = {'title': 'Просмотр поста'}
 
+    def get_object(self, queryset=None):
+        """Метод для счетчика просмотров"""
+        self.object = super().get_object(queryset)
+        self.object.view_count += 1
+        self.object.save()
+        return self.object
 
-class VlogPostDeleteView(DeleteView):
+    def get_queryset(self):
+        """Если не owner или staff, то не может просматривать чужой пост"""
+        queryset = super().get_queryset()
+        if not self.request.user.is_staff:
+            queryset = queryset.filter(owner=self.request.user)
+        return queryset
+
+
+
+class VlogPostDeleteView(LoginRequiredMixin, DeleteView):
     """Контроллер удаления поста"""
     model = VlogPost
     template_name = 'vlog/vlogpost_confirm_delete.html'
